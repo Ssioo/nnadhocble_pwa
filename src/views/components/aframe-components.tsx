ec695c611e08a088c17e2ac340e423db1cf761df
Cho Yeonwoo <wooisso@naver.com>
2021-02-24 13:52:33 +0900

    export ar component

---
+++ src/views/components/aframe-components.tsx
@@ -0,0 +1,68 @@
+/*
+ * Copyright: Copyright (c) 2021. wooisso <yeonwoo.cho@yonsei.ac.kr>
+ * License: MIT
+ * webcross_ar_app from Mobed Laboratory, Yonsei University
+ * Last Updated At 21. 2. 24. 오후 12:50
+ *
+ * @link http://github.com/Ssioo/nnadhoc_ble for the original source repository
+ */
+import React from 'react'
+
+export interface Coord {
+  x: number
+  y: number
+  z: number
+}
+
+export const ARScene: React.FC = (props) => (
+  React.createElement(
+    'a-scene',
+    {
+      'vr-mode-ui': 'enabled: false;',
+      embedded: true,
+      style: {
+        display: 'block',
+        position: 'relative',
+        height: '100%',
+        width: '100%',
+      },
+      renderer: 'logarithmicDepthBuffer: true;',
+      arjs: 'trackingMethod: best; sourceType: webcam; debugUIEnabled: true;'
+    },
+    props.children
+  )
+)
+
+export const ARGLFTEntity: React.FC<{
+  src: string,
+  position: Coord,
+  scale?: Coord
+}> = ({ src, position, scale, children }) => {
+  return React.createElement(
+    'a-glft-model',
+    {
+      src: src,
+      scale: scale ? `${scale.x} ${scale.y} ${scale.z}` : undefined,
+      position: `${position.x} ${position.y} ${position.z}`
+    },
+    children
+  )
+}
+
+export const ARMarker: React.FC<{
+  patternUrl?: string,
+  barcodeUrl?: string,
+  size: number,
+  type: 'pattern' | 'barcode' | 'unknown'
+}> = ({ patternUrl, barcodeUrl, size, type, children }) => {
+  return React.createElement(
+    'a-marker',
+    {
+      url: patternUrl,
+      value: barcodeUrl,
+      size,
+      type,
+    },
+    children
+  )
+}
--- src/views/components/ar-overlay.tsx
+++ src/views/components/ar-overlay.tsx
@@ -1,69 +1,8 @@
 import React from 'react'
 import { DetectedObject } from '@tensorflow-models/coco-ssd'
 import { WINDOW_HEIGHT, WINDOW_WIDTH } from '../../infra/constants'
-import ReactHtmlParser from 'react-html-parser'
 import { observer } from 'mobx-react-lite'
-
-const getArHtml = (modelUrl: string, objects: DetectedObject[]): string => {
-  const result = `
-      <a-scene
-        vr-mode-ui="enabled: false;"
-        embedded style="display: block; position: relative; height: 100%; width: 100%;"
-        renderer="logarithmicDepthBuffer: true;"
-        arjs="trackingMethod: best; sourceType: webcam; debugUIEnabled: true;"
-       >
-          ${objects.map((o) =>
-    `<a-gltf-model
-                src="${modelUrl}"
-                scale="2 2 2"
-                position="${(o.bbox[0] / WINDOW_WIDTH - 0.5) * 20} ${(o.bbox[1] / WINDOW_HEIGHT - 0.5) * 20} ${-50 * WINDOW_WIDTH / o.bbox[2]}"
-            />`
-  ).join('')}
-      </a-scene>`
-  console.log(result)
-  return result
-}
-
-interface Coord {
-  x: number
-  y: number
-  z: number
-}
-
-const ARScene: React.FC = (props) => (
-  React.createElement(
-    'a-scene',
-    {
-      'vr-mode-ui': 'enabled: false;',
-      embedded: true,
-      style: {
-        display: 'block',
-        position: 'relative',
-        height: '100%',
-        width: '100%',
-      },
-      renderer: 'logarithmicDepthBuffer: true;',
-      arjs: 'trackingMethod: best; sourceType: webcam; debugUIEnabled: true;'
-    },
-    props.children
-  )
-)
-
-const ARGLFTEntity: React.FC<{
-  src: string,
-  position: Coord,
-  scale?: Coord
-}> = ({ src, position, scale, children }) => {
-  return React.createElement(
-    'a-glft-model',
-    {
-      src: src,
-      scale: scale ? `${scale.x} ${scale.y} ${scale.z}` : undefined,
-      position: `${position.x} ${position.y} ${position.z}`
-    },
-    children
-  )
-}
+import { ARGLFTEntity, ARScene } from './aframe-components'
 
 export const AROverlay: React.FC<{
   modelUrl: string,
@@ -86,7 +25,6 @@ export const AROverlay: React.FC<{
           />
         )}
       </ARScene>
-      {/*{ReactHtmlParser(getArHtml(modelUrl, objects))}*/}
     </div>
   )
 })
