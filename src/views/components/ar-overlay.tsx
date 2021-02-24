3b298e006d4ed1fc4834d345963344398168fd9a
Cho Yeonwoo <wooisso@naver.com>
2021-02-24 11:20:32 +0900

    gazua

--- src/views/components/ar-overlay.tsx
+++ src/views/components/ar-overlay.tsx
@@ -8,8 +8,8 @@ const getArHtml = (modelUrl: string, objects: DetectedObject[]): string => {
   const result = `
       <a-scene vr-mode-ui="enabled: false;" embedded style="display: block; position: relative; height: 100%; width: 100%;">
           ${objects.map((o) =>
-    `<a-gltf-model src="${modelUrl}" scale="2 2 2" position="${o.bbox[0] / WINDOW_WIDTH - 0.5} ${o.bbox[1] / WINDOW_HEIGHT - 0.5} ${-50 * WINDOW_WIDTH / o.bbox[2]}" />`
-  ).join('')}
+            `<a-gltf-model src="${modelUrl}" scale="2 2 2" position="${(o.bbox[0] / WINDOW_WIDTH - 0.5) * 20} ${(o.bbox[1] / WINDOW_HEIGHT - 0.5) * 20} ${-50 * WINDOW_WIDTH / o.bbox[2]}" />`
+          ).join('')}
       </a-scene>`
   console.log(result)
   return result
